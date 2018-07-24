e8c8fb82644bb9b96b6802d51d73a141b78e990e
Cleber Campomori <cleber.campomori@gmail.com>
2018-07-24 15:30:57 -0300

    Aula 1.1: criando o projeto para conhecermos a Stream API

---
+++ tw-java-streams/src/br/com/treinaweb/java/streams/Empregado.java
@@ -0,0 +1,42 @@
+package br.com.treinaweb.java.streams;
+
+public class Empregado {
+
+	private long id;
+	private String nome;
+	private double salario;
+	private String departamento;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public double getSalario() {
+		return salario;
+	}
+
+	public void setSalario(double salario) {
+		this.salario = salario;
+	}
+
+	public String getDepartamento() {
+		return departamento;
+	}
+
+	public void setDepartamento(String departamento) {
+		this.departamento = departamento;
+	}
+
+}
---
+++ tw-java-streams/src/br/com/treinaweb/java/streams/Main.java
@@ -0,0 +1,9 @@
+package br.com.treinaweb.java.streams;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+	}
+	
+}
