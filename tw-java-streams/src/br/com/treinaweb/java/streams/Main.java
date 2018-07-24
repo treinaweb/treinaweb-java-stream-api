d8100643e5b7ce6d7711db2a5ec4c077343f2c6c
Cleber Campomori <cleber.campomori@gmail.com>
2018-07-24 17:42:11 -0300

    Aula 3.10: lidando com pipelines de streams

--- tw-java-streams/src/br/com/treinaweb/java/streams/Main.java
+++ tw-java-streams/src/br/com/treinaweb/java/streams/Main.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.OptionalDouble;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class Main {
 
@@ -16,11 +15,10 @@ public class Main {
 		empregados.add(new Empregado(4, "Josefina", 7000, "CTO"));
 		
 		System.out.println(" ** Funcionários que começam com J");
-		Stream<Empregado> streamEmpregados = empregados.stream();
-		Stream<Empregado> empregadosComecamComJ = streamEmpregados.filter(emp -> emp.getNome().startsWith("J"));
-		empregadosComecamComJ.forEach((emp) -> System.out.println(emp.getNome()));
-		List<Empregado> empregadosComJ = empregadosComecamComJ.collect(Collectors.toList());
-		empregadosComJ.stream().forEach((emp) -> System.out.println(emp.getNome()));
+		List<Empregado> empregadosComJ = empregados.stream()
+				.filter(emp -> emp.getNome().startsWith("J"))
+				.collect(Collectors.toList());
+		empregadosComJ.forEach((emp) -> System.out.println(emp.getNome()));
 		OptionalDouble menorSalario = empregadosComJ.stream().mapToDouble(emp -> emp.getSalario()).min();
 		if (menorSalario.isPresent()) {
 			System.out.println("Menor salário: R$ " + menorSalario.getAsDouble());
